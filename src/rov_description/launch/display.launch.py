f4d211a8236ea1606ff56a4cc87fddd36d53c181
PFlak <piotr.andrzej.flak@gmail.com>
2025-09-10 20:08:20 +0000

    rov_description

---
+++ src/rov_description/launch/display.launch.py
@@ -0,0 +1,34 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    description_share = FindPackageShare('rov_description')
+
+    default_config_path = PathJoinSubstitution([description_share, 'config', 'params.yaml'])
+    default_rviz_config_path = PathJoinSubstitution([description_share, 'rviz', 'urdf.rviz'])
+
+    config_arg = DeclareLaunchArgument(name="config",
+                                       default_value=default_config_path,
+                                       description="Path to the configuration file for the rov state publisher")
+
+    rviz_config_arg = DeclareLaunchArgument(name="rviz_config",
+                                            default_value=default_rviz_config_path,
+                                            description='Path to the RViz2 configuration file')
+
+    rov_state_publisher_node = Node(package='rov_description',
+                                    executable='rov_state_publisher',
+                                    parameters=[LaunchConfiguration('config')])
+
+    rviz_node = Node(package='rviz2',
+                     executable='rviz2',
+                     output='screen',
+                     arguments=['-d', LaunchConfiguration('rviz_config')])
+
+    return LaunchDescription([config_arg,
+                              rviz_config_arg,
+                              rov_state_publisher_node,
+                              rviz_node])
---
+++ src/rov_description/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = "rov_description"
+
+setup(
+    name=package_name,
+    version="0.0.1",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py*'))),
+        (os.path.join('share', package_name, 'xacro'), glob(os.path.join('xacro', '*.xacro*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz*'))),
+        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*.*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml*'))),
+        (os.path.join('share', package_name, 'nodes'), glob(os.path.join('nodes', '*node.py*')))
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Piotr Flak",
+    maintainer_email="piotrflak@student.agh.edu.pl",
+    description="ROV description files",
+    license="Apache-2.0",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "rov_state_publisher = nodes.rov_state_publisher_node:main"
+        ],
+    },
+)
