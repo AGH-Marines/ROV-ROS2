527d64e4306eeec04cfad8fb6e82ca7b34bd6f54
Piotr Flak <88963612+PFlak@users.noreply.github.com>
2025-09-19 14:48:34 +0200

    ROV-5: Rewrite narval_stonefish -> rov_stonefish (#6)

---
+++ src/simulation/rov_stonefish/README.md
@@ -0,0 +1,47 @@
+# rov_stonefish
+
+## Description
+
+Purpose of this package is to create simulation environment for various scenarios
+
+## Launch
+
+This package has one `*.launch.py` file. 
+
+```bash
+windturbine_bluerov2.launch.py
+wreckage_bluerov2.launch.py
+```
+
+### windturbine_bluerov2.launch.py
+
+- launches `bluerov2` robot description
+- subcribes to `ds4` controller
+- launches `rov_thruster_manager` node
+- launches `windturbine` scene with `bluerov2` robot.
+- opens `rvi2` with `tank_bluerov2_imu.rviz` configuration
+
+#### Parameters
+
+- `config` (default: *windturbine_bluerov2.yaml*) - path to config file
+- `rviz_config` (default: *tank_bluerov2_imu.rviz*) - path to rviz config file
+
+### wreckage_bluerov2.launch.py
+
+Map of wreckage prepared for exhibition
+
+![Map](./docs/images/Screenshot%20from%202025-04-01%2010-11-50.png)
+
+![rviz](./docs/images/Screenshot%20from%202025-04-01%2010-15-25.png)
+
+
+- launches `bluerov2` robot description
+- subcribes to `ds4` controller
+- launches `rov_thruster_manager` node
+- launches `windturbine` scene with `bluerov2` robot.
+- opens `rvi2` with `tank_bluerov2_imu.rviz` configuration
+
+#### Parameters
+
+- `config` (default: *wreckage_bluerov2.yaml*) - path to config file
+- `rviz_config` (default: *tank_bluerov2_imu.rviz*) - path to rviz config file
---
+++ src/simulation/rov_stonefish/setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = "rov_stonefish"
+
+data_files_list = []
+for f in glob(os.path.join("data", "**", "*"), recursive=True):
+    if os.path.isfile(f):  # Only copy files, not directories
+        relative_path = os.path.relpath(f, "data")
+        target_dir = os.path.join("share", package_name, "data")
+        if os.path.dirname(relative_path):  # If inside a subdirectory, preserve it
+            target_dir = os.path.join(target_dir, os.path.dirname(relative_path))
+        data_files_list.append((target_dir, [f]))
+
+setup(
+    name=package_name,
+    version="0.0.1",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py*'))),
+        (os.path.join('share', package_name, 'scenarios'), glob(os.path.join('scenarios', '*.scn*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml*'))),
+        (os.path.join('share', package_name, 'rviz'), glob(os.path.join('rviz', '*.rviz*'))),
+        (os.path.join('share', package_name, 'nodes'), glob(os.path.join('nodes', '*node.py*'))),
+        *data_files_list
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Piotr Flak",
+    maintainer_email="piotrflak@student.agh.edu.pl",
+    description="ROV stonefish simulation",
+    license="Apache-2.0",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [
+            "dvl_pose_translation = nodes.dvl_pose_translation_node:main"
+        ],
+    },
+)
