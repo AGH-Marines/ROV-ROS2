3b1c277b6b08f9311d1154f4555f03c1c918ed6d
n2one <dominikwojtasik191@gmail.com>
2026-01-22 21:05:12 +0000

    stabilize on position task

--- src/autonomy_system/setup.py
+++ src/autonomy_system/setup.py
@@ -14,6 +14,7 @@ setup(
          ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py*'))),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -31,6 +32,8 @@ setup(
         'console_scripts': [
             'behavior_tree = autonomy_system.behavior_tree_node:main',
             'hello_world_action = autonomy_system.behaviours.HelloWorld:main',
+            'stabilize_on_position_action = autonomy_system.behaviours.StabilizeOnPosition:main',
+            'dive_task_action = autonomy_system.behaviours.DiveTask:main',
             'move_to_gate_action = autonomy_system.behaviours.MoveToGate:main',
             'search_gate_action = autonomy_system.behaviours.SearchGate:main',
         ],
